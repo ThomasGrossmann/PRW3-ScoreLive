c96230faf7ac30790df5bb2e4396df8974db22f6
ThomasGrossmann <47849503+ThomasGrossmann@users.noreply.github.com>
2024-02-05 11:50:04 +0100

    Update App.js and server.js with test API call

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.get("/api", (req, res) => {
-    res.message = "Hello from the server!";
+    res.json({ message: "Hello from server!" });
 });
 
 app.listen(PORT, () => {
